7759923f502e07deedc7f13bf25a99a69d1709c9
Thomas Ryszkiewicz <tom.ryszkiewicz@gmail.com>
2026-05-07 10:35:51 -0400

    Corrected the scheme for colors, since UIColor is a 0-1.0 scale for RGB values

--- TranslationTelephone/globals.swift
+++ TranslationTelephone/globals.swift
@@ -8,10 +8,10 @@ import UIKit
 
 struct TTColor {
     static let backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1.0)
-    static let mainText = UIColor(red: 229, green: 229, blue: 229, alpha: 1.0)
-    static let accentColor = UIColor(red: 10, green: 196, blue: 224, alpha: 1.0)
-    static let attentionColor = UIColor(red: 237, green: 136, blue: 20, alpha: 1.0)
-    static let inputColor = UIColor(red: 0, green: 168, blue: 215, alpha: 1.0)
+    static let mainText = UIColor(red: 229/255, green: 229/255, blue: 229/255, alpha: 1.0)
+    static let accentColor = UIColor(red: 10/255, green: 196/255, blue: 224/255, alpha: 1.0)
+    static let attentionColor = UIColor(red: 237/255, green: 136/255, blue: 20/255, alpha: 1.0)
+    static let inputColor = UIColor(red: 0/255, green: 168/255, blue: 215/255, alpha: 1.0)
     static let buttonTextColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1.0)
 }
 
